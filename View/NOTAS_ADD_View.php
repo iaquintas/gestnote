04df1d8fdc955d51ecb79e7ed04a5f63cf238661
Anxo <arinc93@gmail.com>
2017-10-27 01:40:04 +0200

    Cambios chorras de strings
    cambios chorras de strings en addnota, estuve probando porque no me acordaba bien de como funcionaba, mañana a la mañana sigo

--- View/NOTAS_ADD_View.php
+++ View/NOTAS_ADD_View.php
@@ -11,14 +11,14 @@ include '../View/Header.php';?>
       <form name='Form' action='../Controller/NOTAS_Controller.php' method='post'   onsubmit='return comprobar_NOTAS()'>
 
 
-      
-FECHA :<input class = 'tcal' type = 'date' name = 'FECHA' min = '' max = '' value = '' onblur='esVacio(this) && validarFecha(this)' ><br>
-CONTENIDO :<input type ='text' name ='CONTENIDO' size='100'  value='' onblur='esVacio(this) && comprobarText(this,100)' ><br>
-COMPARTIDO :<input type ='text' name ='COMPARTIDO' size='200'  value='' onblur='esVacio(this) && comprobarText(this,200)' ><br>
+
+<?php echo $strings['FECHA'] ?> :<input class = 'tcal' type = 'date' name = 'FECHA' min = '' max = '' value = '' onblur='esVacio(this) && validarFecha(this)' ><br>
+<?php echo $strings['CONTENIDO'] ?>  :<input type ='text' name ='CONTENIDO' size='100'  value='' onblur='esVacio(this) && comprobarText(this,100)' ><br>
+<?php echo $strings['COMPARTIDO'] ?> :<input type ='text' name ='COMPARTIDO' size='200'  value='' onblur='esVacio(this) && comprobarText(this,200)' ><br>
 <input type='submit' name='action' value='ADD'>
 
         </form>
- <a href='../Controller/NOTAS_Controller.php'>Volver </a>
+ <a href='../Controller/NOTAS_Controller.php'><?php echo $strings['Volver'] ?> </a>
         <?php
             include '../View/Footer.php';
           }
